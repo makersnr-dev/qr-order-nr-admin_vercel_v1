b00fe38a526bd3f2a6a79ba13f5acded5627d277
makersnr-dev <cs@make32.com>
2025-09-18 11:59:58 +0900

    Add files via upload

--- api/admin-config.js
+++ api/admin-config.js
@@ -2,10 +2,8 @@ export default async function handler(req, res) {
   const proto = (req.headers['x-forwarded-proto'] || 'https');
   const host = req.headers['x-forwarded-host'] || req.headers['host'];
   const sameOrigin = `${proto}://${host}`;
-
   const apiBase = process.env.API_BASE || sameOrigin;
   const orderBase = process.env.ORDER_BASE || process.env.API_BASE || sameOrigin;
-
-  res.setHeader('Cache-Control', 'no-store');
+  res.setHeader('Cache-Control','no-store');
   res.status(200).json({ apiBase, orderBase });
 }
