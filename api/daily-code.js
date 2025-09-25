e4de86b74e24bb6c52160f950bfa99637ea74f4d
makersnr-dev <cs@make32.com>
2025-09-25 12:20:38 +0900

    Add files via upload

--- api/daily-code.js
+++ api/daily-code.js
@@ -1,12 +1,16 @@
-export default async function handler(req, res){
+export default async function handler(req, res) {
   try{
     const apiBase = process.env.API_BASE;
     if(!apiBase){ res.status(500).send('API_BASE is not set'); return; }
     const url = new URL('/daily-code', apiBase).toString();
-    const h = {}; const a = req.headers['authorization']; if(a) h['authorization'] = a;
+    const h = {};
+    const auth = req.headers['authorization'];
+    if(auth) h['authorization'] = auth;
     const r = await fetch(url, { headers: h });
     const txt = await r.text();
     res.setHeader('Cache-Control','no-store');
     res.status(r.status).send(txt);
-  }catch(e){ res.status(500).send('daily-code proxy error'); }
+  }catch(e){
+    res.status(500).send('daily-code proxy error');
+  }
 }
